11d89dc036671fd97f4157101b37632babb6b633
Paulo Henrique <ph.1928@hotmail.com>
2024-10-11 14:38:17 -0300

    initial commit

---
+++ src/test/java/com/scraping/scrapingmicroservice/ScrapingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scraping.scrapingmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapingMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
