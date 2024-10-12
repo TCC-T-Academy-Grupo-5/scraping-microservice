094b436e9211ea26e6008b10c38d4012f6f5f443
Paulo Henrique <ph.1928@hotmail.com>
2024-10-12 16:49:37 -0300

    chore: change ScrapingController directory
    rename VehicleScrapingRequestDTO to ScrapingRequestDTO

---
+++ src/main/java/com/scraping/scrapingmicroservice/dto/ScrapingRequestDTO.java
@@ -0,0 +1,28 @@
+package com.scraping.scrapingmicroservice.dto;
+
+import com.scraping.scrapingmicroservice.enums.VehicleType;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record ScrapingRequestDTO(
+        @NotNull
+        UUID vehicleId,
+
+        @NotNull
+        VehicleType type,
+
+        @NotNull
+        String brand,
+
+        @NotNull
+        String model,
+
+        @NotNull
+        String year,
+
+        @NotNull
+        String version
+) {
+
+}
