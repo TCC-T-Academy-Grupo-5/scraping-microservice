1046a44d7e7e110e150d44d942722ceb2d82987f
Paulo Henrique <ph.1928@hotmail.com>
2024-10-13 16:35:35 -0300

    refactor: change endpoint name from scrape to deals

--- src/main/java/com/scraping/scrapingmicroservice/controllers/ScrapingController.java
+++ src/main/java/com/scraping/scrapingmicroservice/controllers/ScrapingController.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/scrape")
+@RequestMapping("/deals")
 public class ScrapingController {
 
     private final ObjectFactory<WebDriver> webDriverObjectFactory;
