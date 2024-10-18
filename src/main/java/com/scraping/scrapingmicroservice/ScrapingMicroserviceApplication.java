ae182c13ca59cf4ff38cb5d144666525add38cd5
kingabreel <gabrielyury441@gmail.com>
2024-10-17 21:52:26 -0300

    feat: async config to use multi threading

--- src/main/java/com/scraping/scrapingmicroservice/ScrapingMicroserviceApplication.java
+++ src/main/java/com/scraping/scrapingmicroservice/ScrapingMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.scraping.scrapingmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ScrapingMicroserviceApplication {
 
     public static void main(String[] args) {
